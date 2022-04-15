751229711d031b25a2bb28f8a93f787fb03b29f1
AppDevIn <teamprojectlive@gmail.com>
2022-04-15 19:47:55 +0800

    chore: Refactored get domain command ♻️

--- bot.py
+++ bot.py
@@ -2,14 +2,14 @@ import os
 import json
 from emailGenerate import EmailGenerate
 from flask import Flask, request
-from TelegramBot8 import Message, TeleBot, Update, ParseMode
+from TelegramBot8 import Message, TeleBot, Update, ParseMode, InlineKeyboard, InlineKeyboardButton, CallBackQuery
 
 app = Flask(__name__)
 
 TELE_API_KEY = os.getenv('telegramApiKey')
 RAPID_API_KEY = os.getenv('rapidApiKey')
-bot = TeleBot(TELE_API_KEY)
 
+bot = TeleBot(TELE_API_KEY)
 emailGenerate = EmailGenerate(RAPID_API_KEY)
 
 
@@ -21,18 +21,27 @@ def hi(message: Message):
 @bot.add_command_menu_helper(command="/domain_list", description="Command to get the list of domain")
 def domain_list(message: Message):
     listOfDomain = emailGenerate.get_domains()
-    payload = "<b>List of domains:</b>\n"
+    payload = "<b>List of domains:</b>"
+    keybaords = InlineKeyboard()
+
     for index, mail in enumerate(listOfDomain):
-        payload += f"<b>{index+1}.</b> {mail} \n"
-    bot.send_message(message.chat.id, payload, parse_mode=ParseMode.HTML)
+        button = InlineKeyboardButton(text=mail, callback_data="generate_mail")
+        keybaords.add(button)
+    bot.send_message(message.chat.id, payload, parse_mode=ParseMode.HTML, reply_markup=keybaords)
+
+
+@bot.callback_handler("generate_mail")
+def generate_mail(callback: CallBackQuery):
+    callback.answer("Generating e-mail")
 
 
 @app.route('/', methods=["POST"])
-def hello_world():
+def update():
     bot.start(json.dumps(request.json))
     return 'ok'
 
 
 if __name__ == "__main__":
     bot._set_commands()
+    bot.set_webhook("https://reception-elementary-diana-dan.trycloudflare.com")
     app.run()
